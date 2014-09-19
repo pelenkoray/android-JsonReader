ff4fb2dcc83b02402592105ef933a574e64abdce
Koray Pelen <koray_pelen@hotmail.com>
2014-09-19 16:17:18 +0300

    v2

--- JsonReader/src/com/main/jsonreader/MainActivity.java
+++ JsonReader/src/com/main/jsonreader/MainActivity.java
@@ -3,34 +3,48 @@ package com.main.jsonreader;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private String url1 = "http://api.openweathermap.org/data/2.5/weather?q=";
+	private EditText location, country, temperature, humidity, pressure;
+	private HandleJSON obj;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		location = (EditText) findViewById(R.id.editLocation);
+		country = (EditText) findViewById(R.id.editCountry);
+		temperature = (EditText) findViewById(R.id.editTemp);
+		humidity = (EditText) findViewById(R.id.editHumidity);
+		pressure = (EditText) findViewById(R.id.editPressure);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items
+		// to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	public void open(View view) {
+		String url = location.getText().toString();
+		String finalUrl = url1 + url;
+		country.setText(finalUrl);
+		obj = new HandleJSON(finalUrl);
+		obj.fetchJSON();
+
+		while (obj.parsingComplete)
+			;
+		country.setText(obj.getCountry());
+		temperature.setText(String.valueOf((int) (Float.parseFloat(obj
+				.getTemperature()) - 272.15)) + " C");
+		humidity.setText(obj.getHumidity());
+		pressure.setText(obj.getPressure());
+
+	}
 }
